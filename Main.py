facea1403671510074054e40bba85c72aa95a1e5
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-11-09 13:35:28 -0500

    ContalApp -V 0.1

---
+++ Main.py
@@ -0,0 +1,3 @@
+from View import *
+
+initView()
---
+++ View.py
@@ -0,0 +1,9 @@
+import time
+
+def titleProgram():
+    listLetters = ['C','u','e','n','t','a','l','A','p','p']
+    for z in listLetters:
+        print(f"{z}")
+        time.sleep(0.2)
+
+def menu():
